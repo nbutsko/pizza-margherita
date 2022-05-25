70d420f60ee50a2272c1911d1f4bfcdc934a63ca
Natallia Butsko <n.butsko92@gmail.com>
2022-05-26 00:27:56 +0300

    refactor BasePage

--- src/test/java/pageobjects/BasePage.java
+++ src/test/java/pageobjects/BasePage.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 public class BasePage extends AbstractPage {
@@ -43,6 +46,8 @@ public class BasePage extends AbstractPage {
 
     public CartPage clickButtonCart() {
         buttonCart.click();
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+                .until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@class='cart-button__mini-bag-actions cart-button__mini-bag-actions--bottom']")));
         return new CartPage(driver);
     }
 
