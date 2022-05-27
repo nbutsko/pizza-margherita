1026a38c6ef02cfb774b92ade06287663ef9a754
Natallia Butsko <n.butsko92@gmail.com>
2022-05-27 15:18:31 +0300

    refactor css selectors

--- src/test/java/pageobjects/BasePage.java
+++ src/test/java/pageobjects/BasePage.java
@@ -59,6 +59,7 @@ public class BasePage extends AbstractPage {
                 logger.info("Product added to card " + productTitle);
                 By buttonAddToCardXPath = By.cssSelector("div.product-card__actions>button");
                 product.findElement(buttonAddToCardXPath).click();
+                System.out.println("product " + productTitle);
             }
         }
         return this;
